a4f0035069d7ec1167ddb6df9354b5c98ea93c6c
JieGeLovesDengDuaLang <98467318+JieGeLovesDengDuaLang@users.noreply.github.com>
2022-12-23 12:31:25 +0800

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+
+namespace TownOfThem
+{
+    [BepInPlugin("cn.JieGe.TownOfThem", "Town Of Them", "0.0.0.1")]
+    [BepInProcess("Among Us.exe")]
+    public class Plugin : BasePlugin
+    {
+        public const string ModName = "Town Of Them";
+        public const string ModVer = "0.0.0.1";
+        public const string ModMaker = "JieGe";
+        public const bool IsDev = true;
+
+        public override void Load()
+        {
+            var TownOfThemMessage = new ManualLogSource("Town Of Them");
+            BepInEx.Logging.Logger.Sources.Add(TownOfThemMessage);
+            TownOfThemMessage.LogInfo($"{TownOfThem.Plugin.ModName} is loaded!");
+            if (IsDev) TownOfThemMessage.LogWarning($"v{TownOfThem.Plugin.ModVer} is Dev version!");
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TownOfThem
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "TownOfThem";
+        public const string PLUGIN_NAME = "TownOfThem";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}

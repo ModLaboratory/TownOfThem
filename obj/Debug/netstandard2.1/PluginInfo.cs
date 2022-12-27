af14218a8cf5ec83b94ddd6f8fce3ab7103d70fe
JieGeLovesDengDuaLang <98467318+JieGeLovesDengDuaLang@users.noreply.github.com>
2022-12-27 10:27:32 +0800

    Delete obj directory

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TownOfThem
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TownOfThem";
-        public const string PLUGIN_NAME = "TownOfThem";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}

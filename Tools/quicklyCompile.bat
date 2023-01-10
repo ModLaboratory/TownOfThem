27ad26137d05bc1896cb17e6da8419f9f99cb9e8
JieGeLovesDengDuaLang <98467318+JieGeLovesDengDuaLang@users.noreply.github.com>
2023-01-10 19:05:08 +0800

    添加注释

--- Tools/quicklyCompile.bat
+++ Tools/quicklyCompile.bat
@@ -1,16 +1,18 @@
+::If you want to use it,please edit it:
 @echo off
-set startGamePath=H:\Win7_Desktop\Games
-set gamePath=H:\amgus\AmongUs
-set sourcePath=G:\Among Us Mod\×ÔÖÆÄ£×é\TownOfThem
+set startGamePath=
+::Your game link hereâ†‘(not among us application's path,it's Among Us.url's path)
+set gamePath=
+::Your game path hereâ†‘
+set sourcePath=
+::Your compile path file(an .sln file is in it)
 cd /d %sourcePath%
 dotnet build
 pause
 cd /d %gamePath%
 
 if exist BepInEx copy /y "%sourcePath%\bin\Debug\netstandard2.1\TownOfThem.dll" %gamePath%\BepInEx\plugins\ &echo Press any key to start game...&pause>nul&cd /d %startGamePath% &"Among Us.url" &echo game is starting...&pause
-
+::You maybe need edit the first path in this commandâ†‘
 cd /d %gamePath%
 if not exist BepInEx echo BepInEx missing!&pause
-
-echo ......
-pause
+exit

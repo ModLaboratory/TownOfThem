5ad787f9b3a388a618e575a4169b89b3b033a705
JieGeLovesDengDuaLang <98467318+JieGeLovesDengDuaLang@users.noreply.github.com>
2023-01-11 15:07:48 +0800

    删除无用代码

--- Patches/MainMenuPatches.cs
+++ Patches/MainMenuPatches.cs
@@ -74,13 +74,6 @@ namespace TownOfThem.MainMenuPatches
         {
             __instance.ShowModStamp();
         }
-        /*public static void Postfix(ModManager __instance)
-        {
-            var offset_y = HudManager.InstanceExists ? 1.6f : 0.9f;
-            __instance.ModStamp.transform.position = AspectPosition.ComputeWorldPosition(
-                __instance.localCamera, AspectPosition.EdgeAlignments.RightTop,
-                new Vector3(0.4f, offset_y, __instance.localCamera.nearClipPlane + 0.1f));
-        }*/
     }
 }
     

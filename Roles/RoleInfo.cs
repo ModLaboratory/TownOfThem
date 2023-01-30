1c063a7467c5f4c9f13c5ca1d8b15599dae975c2
JieGeLovesDengDuaLang <98467318+JieGeLovesDengDuaLang@users.noreply.github.com>
2023-01-31 00:48:00 +0800

    Add files via upload

---
+++ Roles/RoleInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TownOfThem.RoleInfo
+{
+    enum RoleId
+    {
+        Sheriff=100,
+        Jester=110,
+        Handicapped=120,
+    }
+    class RoleInfo
+    {
+    }
+}

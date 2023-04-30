bd65509fef4f5517ac425f19df81e822c2580f20
KARPED1EM <leever.zzz@gmail.com>
2023-04-30 09:35:01 +0800

    增加房间超时倒计时

---
+++ Modules/Utils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace TownOfThem;
+public static class Utils
+{
+    public static string ColorString(Color32 color, string str) => $"<color=#{color.r:x2}{color.g:x2}{color.b:x2}{color.a:x2}>{str}</color>";
+
+
+
+
+}
---
+++ Patch/GameStartManagerPatch.cs
@@ -0,0 +1,63 @@
+using AmongUs.Data;
+using AmongUs.GameOptions;
+using HarmonyLib;
+using InnerNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TownOfThem;
+
+[HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Update))]
+public static class GameStartManagerUpdatePatch
+{
+    public static void Prefix(GameStartManager __instance)
+    {
+        __instance.MinPlayers = 1;
+    }
+}
+
+public class GameStartManagerPatch
+{
+    private static float timer = 600f;
+    [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Start))]
+    public class GameStartManagerStartPatch
+    {
+        public static void Postfix(GameStartManager __instance)
+        {
+            timer = 600f;
+        }
+    }
+
+    [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Update))]
+    public class GameStartManagerUpdatePatch
+    {
+        private static bool update = false;
+        private static string currentText = "";
+        public static void Prefix(GameStartManager __instance)
+        {
+            // Lobby code
+            if (!AmongUsClient.Instance.AmHost || !GameData.Instance || AmongUsClient.Instance.NetworkMode == NetworkModes.LocalGame) return; // Not host or no instance or LocalGame
+            update = GameData.Instance.PlayerCount != __instance.LastPlayerCount;
+        }
+        public static void Postfix(GameStartManager __instance)
+        {
+            if (!AmongUsClient.Instance) return;
+
+            // Lobby timer
+            if (!AmongUsClient.Instance.AmHost || !GameData.Instance) return;
+
+            if (update) currentText = __instance.PlayerCounter.text;
+
+            timer = Mathf.Max(0f, timer -= Time.deltaTime);
+            int minutes = (int)timer / 60;
+            int seconds = (int)timer % 60;
+            string suffix = $" ({minutes:00}:{seconds:00})";
+            if (timer <= 60) suffix = Utils.ColorString(Color.red, suffix);
+
+            __instance.PlayerCounter.text = currentText + suffix;
+            __instance.PlayerCounter.autoSizeTextContainer = true;
+        }
+    }
+}
